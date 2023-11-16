52f584bac237d6f5dc7790963b600546fa93e171
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2023-11-15 23:11:33 -0400

    script acceptance criteria gathered requirement design adde

--- script.js
+++ script.js
@@ -14,14 +14,25 @@ function writePassword(generateBtn) {
 
 // Password Generator function with criteria
 function generatePassword() {
- 
-    // present prompts: lenght of password
-    //present prompts: character type
-    //generate password
+
+    // Need to present user with a series of prompts for password criteria
+
+    // 1st - Prompt user for the length of the password and
+    // check that length is at least 8 characters and no more than 128 character
+
+    // Ask user for character types to include in the password
+    // Confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+    // Once user answer to all promts
+    // Then validate input with at least one character type that should be selected
+
+    //given all prompts are answered
+    // generate password that matches the selected criteria
+
+    // generate password and either display it in alert or written on page
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 

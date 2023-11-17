a8ab4b27fe6f734d034f8412247d002e6a22242e
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2023-11-17 18:36:05 -0400

    created state flow diagram and some JS functions

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,42 +1,36 @@
-// Assignment Code
+
+// Assignment code  (id = generate) to generateBtn variable
 var generateBtn = document.querySelector("#generate");
 
-console.log()
+// Declare global variables
 
 // Write password to the #password input
-function writePassword(generateBtn) {
+function writePassword() {
+
+    var passwordLength = promptPasswordLength(); // 
+    console.log(passwordLength)
     var password = generatePassword();
+    console.log(password)
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-    // display value
-    console.log(passwordText.value)
+}
+
+// Prompt user for the length of the password
+function promptPasswordLength() {
+    do {
+        passwordLengthValue = prompt("How long would you like your password to be? (Enter a number between 8 and 128)");
+        passwordLengthValue = parseInt(passwordLength);
+    } while (isNaN(passwordLengthValue) || passwordLengthValue < 8 || passwordLengthValue > 128);
+    console.log(passwordLengthValue);
+    return passwordLengthValue;
 }
 
 // Password Generator function with criteria
 function generatePassword() {
 
-    // Need to present user with a series of prompts for password criteria
-    
-    // 1st - Prompt user for the length of the password and
-    // check that length is at least 8 characters and no more than 128 character
-    // --> ask user: How long would you like your password to be?
-    // --> then indicate to: Please choose a number between 8 characters to 128 characters.
-
-    // Ask user for character types to include in the password
-    // Confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-    // make prompt that includes all of the above via checkmark
-
-    // Once user answer to all promts
-    // Then validate input with at least one character type that should be selected
-
-    //given all prompts are answered
-    // generate password that matches the selected criteria
-
-    // generate password and either display it in alert or written on page
-
 }
 
-// Add event listener to generate button
+// Add event listener for button "Generate Password"
 generateBtn.addEventListener("click", writePassword);
 
